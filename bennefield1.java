466556e77a111220c1661ba41396cc1dcebac70c
unknown <kbennefield14@gmail.com>
2023-09-20 12:50:52 -0500

    add problem 1

---
+++ bennefield1.java
@@ -0,0 +1,15 @@
+//233168
+public class bennefield1 {
+	public static void main(String[] args) {
+		int limit = 1000;
+		int sum = 0;
+
+		for (int i = 1; i < limit; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				sum += i;
+			}
+		}
+
+		System.out.println(sum);
+	}
+}
