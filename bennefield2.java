21527bf19f26c33098a9a4a1d54475d133292ef7
unknown <kbennefield14@gmail.com>
2023-09-25 16:54:56 -0500

    added bennefield2

---
+++ bennefield2.java
@@ -0,0 +1,20 @@
+public class bennefield2 {
+    public static void main(String[] args) {
+        int limit = 4000000;
+        long sum = 0;
+        int prev = 1;
+        int current = 1;
+
+        while (current <= limit) {
+            if (current % 2 == 0) {
+                sum += current;
+            }
+
+            int next = prev + current;
+            prev = current;
+            current = next;
+        }
+
+        System.out.println("The sum of even Fibonacci terms not exceeding 4 million is: " + sum);
+    }   
+}
