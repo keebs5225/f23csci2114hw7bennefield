4d1a15f4866e500cdea116552ddada1f2625744b
unknown <kbennefield14@gmail.com>
2023-10-04 08:58:34 -0500

    added problem 6

---
+++ bennefield6.java
@@ -0,0 +1,18 @@
+//answer
+public class bennefield6 {
+	public static void main(String[] args) {
+		int n = 100;
+		int sumOfSquares = 0;
+		int squareOfSum = 0;
+
+		for (int i = 1; i <= n; i++) {
+            sumOfSquares += i * i;
+            squareOfSum += i;
+        }
+
+        squareOfSum = squareOfSum * squareOfSum;
+
+        int difference = squareOfSum - sumOfSquares;
+        System.out.println(difference);
+	}
+}
