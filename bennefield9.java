629e254afe48173e744577e228ba52bb6cc3b0f7
unknown <kbennefield14@gmail.com>
2023-10-04 09:00:11 -0500

    added problem 9

---
+++ bennefield9.java
@@ -0,0 +1,17 @@
+//A
+public class bennefield9 {
+    public static void main(String[] args) {
+        int targetSum = 1000;
+
+        for (int a = 1; a < targetSum; a++) {
+            for (int b = a + 1; b < targetSum; b++) {
+                int c = targetSum - a - b;
+                if (a * a + b * b == c * c) {
+                    int product = (int) a * b * c;
+                    System.out.println(product);
+                    return; 
+                }
+            }
+        }
+    }
+}
