860459d3faa4422bfe7ac5399c82e75596cbc77c
unknown <kbennefield14@gmail.com>
2023-10-04 08:59:33 -0500

    added problem 7

---
+++ bennefield7.java
@@ -0,0 +1,40 @@
+//A
+public class bennefield7 {
+	public static void main(String[] args) {
+
+		int n = 10001;
+		int count = 1;
+		int primeNum = 3;
+
+		while (count < n) {
+			if (isPrime(primeNum)) {
+				count++;
+				if (count == n) {
+					System.out.println(primeNum);
+					break;
+				}
+			}
+		}
+	}
+
+	static boolean isPrime(int num) {//find prime num
+	if (num <= 1) {
+		return false;
+    	}
+	if (num == 2) {
+        return true;
+    }
+    if (num % 2 == 0) {
+        return false;
+    }
+
+    // Check for prime by dividing only by odd numbers up to the square root of num
+    for (int i = 3; i * i <= num; i += 2) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+	}
+}
